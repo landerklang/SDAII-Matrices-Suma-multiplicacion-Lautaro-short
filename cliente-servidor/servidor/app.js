51cc2059564a81e4869c1bd567c124914d751d70
¨lautaro <shortlauty35@gmail.com>
2026-04-15 18:34:00 -0300

    se añadio la funcion de multiplicar las matrices

--- cliente-servidor/servidor/app.js
+++ cliente-servidor/servidor/app.js
@@ -11,12 +11,36 @@ app.post("/calcular", (req, res) => {
 
   let resultado = [];
 
+  // SUMA
   if (operacion === "suma") {
-    for (let i = 0; i <= matriz1.length; i++) {
+    for (let i = 0; i < matriz1.length; i++) {
       let fila = [];
 
-      for (let j = 0; j <= matriz1[i].length; j++) {
+      for (let j = 0; j < matriz1[i].length; j++) {
         fila.push(matriz1[i][j] + matriz2[i][j]);
+      }
+
+      resultado.push(fila);
+    }
+  }
+
+  // MULTIPLICACIÓN REAL
+  else if (operacion === "multiplicacion") {
+    let filasA = matriz1.length;
+    let columnasA = matriz1[0].length;
+    let columnasB = matriz2[0].length;
+
+    for (let i = 0; i < filasA; i++) {
+      let fila = [];
+
+      for (let j = 0; j < columnasB; j++) {
+        let suma = 0;
+
+        for (let k = 0; k < columnasA; k++) {
+          suma += matriz1[i][k] * matriz2[k][j];
+        }
+
+        fila.push(suma);
       }
 
       resultado.push(fila);
